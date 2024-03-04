b3190fabb2fb2c7b5e30ca3c30a1bba52e329138
zillent <serg@zillent.ru>
2024-03-04 23:28:44 +0500

    add constructor, getters and setters

--- src/main/java/ru/zillent/study/account/Account.java
+++ src/main/java/ru/zillent/study/account/Account.java
@@ -3,12 +3,41 @@ package ru.zillent.study.account;
 
 import java.util.HashMap;
 
+import static ru.zillent.study.account.Currency.*;
+
 public class Account {
+
     private String accountant;
     private HashMap<Currency, Integer> balance;
 
+    public Account(String accountant) {
+        this.accountant = accountant;
+        this.balance = new HashMap<>();
+    }
+
+    public String getAccountant() {
+        return accountant;
+    }
+
+    public void setAccountant(String accountant) {
+        this.accountant = accountant;
+    }
+
+    public HashMap<Currency, Integer> getBalance() {
+        return new HashMap<>(balance);
+    }
+
+    public void setBalance(Currency currency, int amount) {
+        if (currency == null) throw new IllegalArgumentException("Wrong currency");
+        if (amount<0) throw new IllegalArgumentException("Amount must be positive");
+        balance.put(currency, amount);
+    };
+
     public static void main(String[] args) {
         System.out.println("Test");
-        return;
+        Account account = new Account("Test");
+        account.setBalance(RUB,33);
+        account.setBalance(USD,33);
+        System.out.println(account.getBalance());
     }
 }
